f87b27f8b6563975d731c6dcebd44ee9c142ee5b
Benjafo <benjafo@gmail.com>
2025-09-21 15:13:33 -0400

    Prettier formatting

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,15 +1,23 @@
-import express, { Router } from 'express';
-import * as authController from '../controllers/authController';
-import { authenticateToken } from '../middleware/auth';
-import { validateData } from '../middleware/validation';
-import { signInSchema, requestPasswordResetSchema, createPasswordSchema } from '../schemas/authSchemas';
+import express, { Router } from "express";
+import * as authController from "../controllers/authController";
+import { authenticateToken } from "../middleware/auth";
+import { validateData } from "../middleware/validation";
+import {
+    signInSchema,
+    requestPasswordResetSchema,
+    createPasswordSchema,
+} from "../schemas/authSchemas";
 
 const router: Router = express.Router();
 
-router.post('/sign-in', validateData(signInSchema), authController.signIn);
-router.post('/sign-out', authenticateToken, authController.signOut);
-router.post('/reset-password', validateData(createPasswordSchema), authController.resetPassword);
-router.post('/token-refresh', authController.refreshToken);
-router.post('/request-password-reset', validateData(requestPasswordResetSchema), authController.requestPasswordReset);
+router.post("/sign-in", validateData(signInSchema), authController.signIn);
+router.post("/sign-out", authenticateToken, authController.signOut);
+router.post("/reset-password", validateData(createPasswordSchema), authController.resetPassword);
+router.post("/token-refresh", authController.refreshToken);
+router.post(
+    "/request-password-reset",
+    validateData(requestPasswordResetSchema),
+    authController.requestPasswordReset
+);
 
 export default router;
