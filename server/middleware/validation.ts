d08f5f4508ce88a5684d6d4f6f9b0be4bba5fb83
Benjafo <benjafo@gmail.com>
2025-09-21 15:01:56 -0400

    Fix any type issue

--- server/middleware/validation.ts
+++ server/middleware/validation.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { z, ZodError } from "zod";
 
-export function validateData(schema: z.ZodObject<any, any>) {
+export function validateData(schema: z.ZodObject) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             schema.parse(req.body);
