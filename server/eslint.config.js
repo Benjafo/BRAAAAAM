e8507404902c2f7e52df518241548140aff01f95
Benjafo <benjafo@gmail.com>
2025-09-21 14:50:07 -0400

    Update eslint rules

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import typescript from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
+const globals = require("globals");
 
 export default [
     js.configs.recommended,
@@ -12,12 +13,16 @@ export default [
                 ecmaVersion: 2022,
                 sourceType: "module",
             },
+            globals: {
+                ...globals.node,
+            },
         },
         plugins: {
             "@typescript-eslint": typescript,
         },
         rules: {
             ...typescript.configs.recommended.rules,
+            "@typescript-eslint/no-require-imports": "off",
             "@typescript-eslint/no-explicit-any": "error",
             "@typescript-eslint/explicit-function-return-type": "warn",
             "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
