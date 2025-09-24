3d491d708cbffebc9279648778422fec509cabab
Benjafo <benjafo@gmail.com>
2025-09-24 17:44:22 -0400

    Add static admin dashboard

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
 import { MainNavigation } from "@/components/Navigation";
+import { useAuthStore } from "@/components/stores/authStore";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
+    beforeLoad: async () => {
+        const { isAuthenticated } = useAuthStore.getState();
+
+        if (isAuthenticated) {
+            throw redirect({
+                to: "/dashboard",
+            });
+        }
+    },
     component: RouteComponent,
 });
 
