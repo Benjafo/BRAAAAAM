fefa7cd118a6a7c3faa3f6ab91c698e25d7d6039
Miles Krassen <msk4480@rit.edu>
2025-11-21 01:01:45 -0500

    Fixed permission routing issue with dashboard.

--- client/src/routes/{-$subdomain}/_auth/dashboard.tsx
+++ client/src/routes/{-$subdomain}/_auth/dashboard.tsx
@@ -2,36 +2,47 @@ import AdminDashboard from "@/components/AdminDashboardPage";
 import { MainNavigation } from "@/components/Navigation";
 import { authStore } from "@/components/stores/authStore";
 import { PERMISSIONS } from "@/lib/permissions";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/{-$subdomain}/_auth/dashboard")({
-    beforeLoad: async () => {
-        const s = authStore.getState();
-        // const isAuthed = Boolean(s.user && s.accessToken);
+    // beforeLoad: async () => {
+    //     const s = authStore.getState();
+    //     // const isAuthed = Boolean(s.user && s.accessToken);
 
-        // if (!isAuthed) {
-        //     throw redirect({
-        //         to: "/{-$subdomain}/sign-in",
-        //         search: { redirect: location.pathname },
-        //     });
-        // }
+    //     // if (!isAuthed) {
+    //     //     throw redirect({
+    //     //         to: "/{-$subdomain}/sign-in",
+    //     //         search: { redirect: location.pathname },
+    //     //     });
+    //     // }
 
-        if (!s.hasPermission(PERMISSIONS.DASHBOARD_READ)) {
-            throw redirect({
-                to: "/{-$subdomain}",
-            });
-        }
+    //     if (!s.hasPermission(PERMISSIONS.DASHBOARD_READ)) {
+    //         throw redirect({
+    //             to: "/{-$subdomain}/schedule",
+    //         });
+    //     }
 
-        return { user: s.user };
-    },
+    //     return { user: s.user };
+    // },
     component: RouteComponent,
 });
 
 function RouteComponent() {
+
+    const s = authStore.getState()
+
     return (
         <div className="">
             <MainNavigation />
-            <AdminDashboard />
+            {s.hasPermission(PERMISSIONS.DASHBOARD_READ) ? (
+                <AdminDashboard />
+            ) : (
+                <div className="m-4">
+                    <div className="text-3xl font-bold mb-2">Welcome, {s.user?.firstName} {s.user?.lastName}!</div>
+                    <p>Nothing to display on your dashboard.</p>
+                </div>
+            )}
+            {/* <AdminDashboard /> */}
         </div>
     );
 }
